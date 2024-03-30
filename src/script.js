bacc2e90ee785bc4d6e28beb41164ba4dad3e839
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-31 02:45:35 +0530

    Toggle Classes:
    banner.classList.toggle("bg-gray-900"): This toggles the class bg-gray-900 on the element with the class banner, changing its background color to gray when the dayNight element is clicked.
    banner.classList.toggle("text-white"): This toggles the class text-white on the element with the class banner, changing its text color to white when the dayNight element is clicked.
    day.classList.toggle("hidden"): This toggles the class hidden on the element with the class day, making it visible or hidden when the dayNight element is clicked.
    night.classList.toggle("hidden"): This toggles the class hidden on the element with the class night, making it visible or hidden when the dayNight element is clicked.

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ let night =  document.querySelector(".night");
 dayNight.addEventListener("click", () => {
     banner.classList.toggle("bg-gray-900");
     banner.classList.toggle("text-white");
+    
     // Add or toggle other classes as needed
     day.classList.toggle("hidden");
     night.classList.toggle("hidden");
