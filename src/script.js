ce526a6152a5b278101bee975f172e19f66cf15e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-31 02:44:35 +0530

    Query Selectors:
    document.querySelector(".dayNight"): This selects the element with the class dayNight.
    document.querySelector(".banner"): This selects the element with the class banner.
    document.querySelector(".day"): This selects the element with the class day.
    document.querySelector(".night"): This selects the element with the class night.

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let dayNight =  document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+let day =  document.querySelector(".day");
+let night =  document.querySelector(".night");
+
