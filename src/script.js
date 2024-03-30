2153dc667c404678e463811a9de40430e12390f7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-30 12:27:52 +0530

    Event Listeners:
    An event listener is added to the btn element. When clicked, it triggers the getWeather(city) function, which fetches weather data based on the input city.
    Another event listener is added to the input element to listen for keydown events. When the "Enter" key is pressed, it also triggers the getWeather(city) function, allowing the user to submit the form by pressing "Enter" instead of clicking the button.

--- src/script.js
+++ src/script.js
@@ -4,3 +4,15 @@ const icon= document.querySelector(".icon");
 const weather= document.querySelector(".weather")
 const temperature= document.querySelector(".temperature")
 const description= document.querySelector(".description")
+
+btn.addEventListener("click",()=>{
+    let city = input.value;
+    getWeather(city);
+})
+
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        let city = input.value;
+        getWeather(city);
+    }
+  });
