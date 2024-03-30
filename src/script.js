b9e5793174798cbf906a5ed0ab449fc992129e30
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-31 03:58:04 +0530

    Validation Criteria:
    Username: Checks if it's empty.
    Email: Checks if it's empty and if it matches a valid email format.
    Password: Checks if it's empty and if it's at least 8 characters long.
    Confirm Password: Checks if it matches the password.

--- src/script.js
+++ src/script.js
@@ -11,3 +11,39 @@ form.addEventListener('submit', e => {
     validateInputs();
 });
 
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const cPasswordValue = cPassword.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 characters.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(cPasswordValue === '') {
+        setError(cPassword, 'Please confirm your password');
+    } else if (cPasswordValue !== passwordValue) {
+        setError(cPassword, "Passwords don't match");
+    } else {
+        setSuccess(cPassword);
+    }
+};
