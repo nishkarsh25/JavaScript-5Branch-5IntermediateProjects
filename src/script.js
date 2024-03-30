d0db011b8f641ca632296b48b926830cb84f7847
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-03-30 12:29:08 +0530

    getWeather(city) Function:
    This function takes a city parameter.
    It constructs a URL using the OpenWeatherMap API to fetch weather data for the specified city.
    It fetches the data using fetch() and processes it as JSON.
    Once the data is received, it extracts relevant information such as the weather icon, city name, country, temperature, and description.
    The extracted data is then used to update the corresponding HTML elements (icon, weather, temperature, and description) with the fetched weather information.

--- src/script.js
+++ src/script.js
@@ -16,3 +16,32 @@ input.addEventListener("keydown", (event) => {
         getWeather(city);
     }
   });
+
+ function getWeather(city){
+    console.log(city);
+
+ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a52e689a44c99bdb44350cf002da7b76`)
+ .then(response=>response.json())
+ .then(data => {
+    console.log(data);
+
+    const iconCode= data.weather[0].icon;
+    icon.innerHTML=`<img src="http://openweathermap.org/img/wn/${iconCode}.png" alt="Weather Icon"/>`
+
+    const weatherCity = data.name;
+    const weatherCountry = data.sys.country;
+    weather.innerHTML=`${weatherCity}, ${weatherCountry}`;
+
+
+     let weatherTemp = data.main.temp;
+     weatherTemp=  weatherTemp - 273;
+     const temp = weatherTemp.toFixed(2)
+     temperature.innerHTML= `${temp}°C`;
+
+    const weatherDesc = data.weather[0].description;
+    description.innerHTML= weatherDesc;
+
+
+})
+
+}
